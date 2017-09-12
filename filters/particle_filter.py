d8c13df57203780f6866a44a2e468d9b16edba7a
Talha Havadar <talha.havadar@telenetgroup.be>
2017-09-12 11:10:42 +0300

    import fixed

--- filters/particle_filter.py
+++ filters/particle_filter.py
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-from filters.filter import Filter, Vector2D
+from robot_localization.filters.filter import Filter, Vector2D
 
 class ParticleFilter(Filter):
 
